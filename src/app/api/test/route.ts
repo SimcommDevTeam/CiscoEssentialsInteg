15ee878a7b705c4c7fc30466f0e2cc53ab660eb4
SimcommDevTeam <devteam@simcomm.com>
2026-05-27 13:59:36 +0530

    added standalone output

---
+++ src/app/api/test/route.ts
@@ -0,0 +1,11 @@
+export const dynamic = "force-dynamic";
+export const runtime = "nodejs";
+
+export async function GET() {
+
+  return Response.json({
+    token: process.env.SALESFORCE_BEARER_TOKEN
+      ? "FOUND"
+      : process.env.SALESFORCE_BEARER_TOKEN
+  });
+}
